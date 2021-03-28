485a3ea1bbe3ca7faea654dcf12fde87edd7900d
Nikolaj <Nikodyring@gmail.com>
2021-03-28 21:30:02 +0200

    changed profile picture for mobile version

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,7 +12,7 @@
         <div
           class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center"
           style="
-            background-image: url('https://images.unsplash.com/photo-1478432780021-b8d273730d8c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2700&q=80');
+            background-image: url('../assets/ProfilePicture.webp');
           "
         ></div>
 
