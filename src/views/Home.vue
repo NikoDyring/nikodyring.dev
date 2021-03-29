cfd5cafc34a086053d03d6743c0744f31934c67b
Nikolaj <Nikodyring@gmail.com>
2021-03-29 16:05:35 +0200

    darkmode reposition

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,7 +16,7 @@
           "
         ></div>
 
-        <h1 class="text-3xl font-bold pt-6 lg:pt-0"><button class="h-8 m-2 js-change-theme focus:outline-none">🌙</button>Nikolaj Dyring Jensen</h1>
+        <h1 class="text-3xl font-bold pt-6 lg:pt-0">Nikolaj Dyring Jensen</h1>
         <div
           class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"
         ></div>
@@ -64,6 +64,7 @@
         <div
           class="mt-6 pb-16 lg:pb-0 w-4/5 lg:w-full mx-auto flex flex-wrap items-center justify-between"
         >
+        <button class="h-8 m-2 js-change-theme focus:outline-none">🌙</button>
           <a class="link-no-tippy" href="https://www.facebook.com/NDyring/"
             ><svg
               class="h-6 fill-current text-gray-600 hover:text-green-700"
