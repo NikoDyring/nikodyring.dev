7034d2142f39461d78b084bf09014cef4131c393
NikoDyring <nikodyring@gmail.com>
2023-03-02 02:41:03 +0100

    Add home page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,29 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+const lightSwitches = document.querySelectorAll('.light-switch');
+if (lightSwitches.length > 0) {
+    lightSwitches.forEach((lightSwitch, i) => {
+        if (localStorage.getItem('dark-mode') === 'true') {
+            // eslint-disable-next-line no-param-reassign
+            lightSwitch.checked = true;
+        }
+        lightSwitch.addEventListener('change', () => {
+            const { checked } = lightSwitch;
+            lightSwitches.forEach((el, n) => {
+                if (n !== i) {
+                    // eslint-disable-next-line no-param-reassign
+                    el.checked = checked;
+                }
+            });
+            if (lightSwitch.checked) {
+                document.documentElement.classList.add('dark');
+                localStorage.setItem('dark-mode', true);
+            } else {
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('dark-mode', false);
+            }
+        });
+    });
+}
